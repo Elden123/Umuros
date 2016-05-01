eaacdea18d7c73c0e8ea33549121734d4528b8f9
dash102 <stephaniefu@rocketmail.com>
2016-05-01 12:50:18 -0500

    I <3 Hack K-State
    All Done

--- MyApplication/app/src/main/java/com/example/nolan/myapplication/GifView.java
+++ MyApplication/app/src/main/java/com/example/nolan/myapplication/GifView.java
@@ -1,24 +1,3 @@
-//if (m.moodOfTwitter == ("Happy"))
-//if (colors.colorChange == ("Romantic"))
-//is = getResources().openRawResource(+R.mipmap.turquoise_resized);
-           /* else if (colors.colorChange == ("Sad"))
-                is = getResources().openRawResource(+R.mipmap.gray_resized);
-            else if (colors.colorChange == ("Positive"))
-                is = getResources().openRawResource(+R.mipmap.green_resized);
-            else if (colors.colorChange == ("Angry"))
-                is = getResources().openRawResource(+R.mipmap.red_resized);
-            else if (colors.colorChange == ("Strong"))
-                is = getResources().openRawResource(+R.mipmap.dark_gray_resized);
-            else if (colors.colorChange == ("Afraid"))
-                is = getResources().openRawResource(+R.mipmap.carrot_resized);
-            else if (colors.colorChange == ("Confused"))
-                is = getResources().openRawResource(+R.mipmap.orange_resized);
-            else if (colors.colorChange == ("Open"))
-                is = getResources().openRawResource(+R.mipmap.purple_resized);
-            else is = getResources().openRawResource(+R.mipmap.blue_resized);
-
-        }*/
-//mMovie = Movie.decodeStream(is);
 package com.example.nolan.myapplication;
 
 import java.io.InputStream;
@@ -39,22 +18,23 @@ public class GifView extends View {
 
     public GifView(Context context) {
         super(context);
-        initializeView(+R.mipmap.blue_resized);
+        initializeView();
     }
 
     public GifView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        initializeView(+R.mipmap.blue_resized);
+        initializeView();
     }
 
     public GifView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
-        initializeView(+R.mipmap.blue_resized);
+        initializeView();
     }
 
-    private void initializeView(int i) {
+    private void initializeView() {
 //R.drawable.loader - our animated GIF
-        InputStream is = getResources().openRawResource(i);
+        MainActivity m = new MainActivity();
+        InputStream is = getResources().openRawResource(+R.mipmap.bluehex_final_resized);
         mMovie = Movie.decodeStream(is);
     }
 
@@ -77,7 +57,7 @@ public class GifView extends View {
 
     public void setGIFResource(int resId) {
         this.gifId = resId;
-        initializeView(+R.mipmap.blue_resized);
+        initializeView();
     }
 
     public int getGIFResource() {
